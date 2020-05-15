7ca19dcb57eab20e3179baee2ac8425bed17c5fb
Melanie Follett <groves.melanie@gmail.com>
2020-05-14 20:55:10 -0700

    Completed Web App

--- Web App/app.py
+++ Web App/app.py
@@ -4,6 +4,7 @@ from flask import Flask, request, render_template
 import requests
 from web_scraper import commentScraper
 from data_cleaning import calculateModelParameters
+from model import personalityTypeResult
 
 app = Flask(__name__)
 
@@ -19,9 +20,18 @@ def predict():
         comments = commentScraper(username)
 
         scores = calculateModelParameters(username)
-        
-        return (render_template('result.html', username=username, comments=comments, scores=scores))
 
+        personality = personalityTypeResult(username)
+        
+        return (render_template('result.html', username=username, comments=comments, scores=scores, personality=personality))
+
+@app.route('/data')
+def data():
+    return (render_template('data.html'))
+
+@app.route('/about')
+def about():
+    return (render_template('about.html'))
 
 # @app.route('/result',methods = ['POST', 'GET'])
 # def get_input():
